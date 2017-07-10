879f96a62b741040d6f86e3e4a8a677020049a4f
elison <elison@voopter.com>
2017-07-10 16:50:10 -0300

    Criação do novo projeto para o branch login. Projeto base criado pelo Android Studio

---
+++ app/src/main/java/br/com/bg7/appvistoria/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.bg7.appvistoria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ *   MainActivity of Application
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
