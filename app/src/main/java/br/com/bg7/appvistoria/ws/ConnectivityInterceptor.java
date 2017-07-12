5c25505b95949b921d417fb3e0d6ee37da4aeb4b
elison <elison@voopter.com>
2017-07-12 17:49:19 -0300

    Criação das classes pra interceptar o status da conexao ao efetuar um request

---
+++ app/src/main/java/br/com/bg7/appvistoria/ws/ConnectivityInterceptor.java
@@ -0,0 +1,41 @@
+package br.com.bg7.appvistoria.ws;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by: elison
+ * Date: 2017-07-12
+ */
+public class ConnectivityInterceptor implements Interceptor {
+
+    private Context mContext;
+
+    public ConnectivityInterceptor(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        if (!isOnline(mContext)) {
+            throw new NoConnectivityException();
+        }
+
+        Request.Builder builder = chain.request().newBuilder();
+        return chain.proceed(builder.build());
+    }
+
+    public static boolean isOnline(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = connectivityManager.getActiveNetworkInfo();
+        return (netInfo != null && netInfo.isConnected());
+    }
+
+}
--- app/src/main/java/br/com/bg7/appvistoria/ws/RetrofitClient.java
+++ app/src/main/java/br/com/bg7/appvistoria/ws/RetrofitClient.java
@@ -1,5 +1,7 @@
 package br.com.bg7.appvistoria.ws;
 
+import br.com.bg7.appvistoria.Applic;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -15,8 +17,14 @@ public class RetrofitClient {
 
     public static Retrofit getClient(String baseUrl) {
         if (retrofit==null) {
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(new ConnectivityInterceptor(Applic.getInstance().getApplicationContext()))
+                    .build();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(baseUrl)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
