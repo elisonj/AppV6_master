6558e2037688eb5252d891b8a321f2a61bc5b28e
Lucio Assis <lucio.assis@bg7.com.br>
2017-07-14 21:15:01 -0300

    Grande refactoring do Login para usar MVP

--- app/src/main/java/br/com/bg7/appvistoria/MainActivity.java
+++ app/src/main/java/br/com/bg7/appvistoria/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import br.com.bg7.appvistoria.activity.LoginActivity;
+import br.com.bg7.appvistoria.login.LoginActivity;
 
 /**
  * Created by: elison
