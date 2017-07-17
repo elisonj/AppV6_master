20a64aa018e7b85e5b6c20d760b6562a9701b60b
elison <elison@voopter.com>
2017-07-17 17:30:56 -0300

    Criado uma BaseActivity para cuidar da alteração da linguagem nas Activits, atraves do LanguageContextWrap

---
+++ app/src/main/java/br/com/bg7/appvistoria/BaseActivity.java
@@ -0,0 +1,23 @@
+package br.com.bg7.appvistoria;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by: elison
+ * Date: 2017-07-17
+ */
+public class BaseActivity extends AppCompatActivity {
+    private String defaultLanguage;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(newBase);
+        defaultLanguage = preferences.getString("Language", "pt");
+
+        super.attachBaseContext(LanguageContextWrap.wrap(newBase, defaultLanguage));
+    }
+}
