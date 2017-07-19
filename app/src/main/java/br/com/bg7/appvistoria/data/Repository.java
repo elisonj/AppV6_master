06e03a17e043b01fa3c0c0c4ff04078fdc2666e7
Lucio Assis <lucio.assis@bg7.com.br>
2017-07-19 14:27:44 -0300

    Implementar Repository com Sugar

--- app/src/main/java/br/com/bg7/appvistoria/data/Repository.java
+++ app/src/main/java/br/com/bg7/appvistoria/data/Repository.java
@@ -2,6 +2,8 @@ package br.com.bg7.appvistoria.data;
 
 import com.orm.SugarRecord;
 
+import java.util.Iterator;
+
 /**
  * Created by: luciolucio
  * Date: 2017-07-18
@@ -9,18 +11,25 @@ import com.orm.SugarRecord;
 
 public abstract class Repository<T extends SugarRecord<T>> {
     public T save(T entity) {
-        return null;
+        entity.save();
+        return entity;
     }
 
-    public T first() {
-        return null;
+    public T first(Class<T> type) {
+        Iterator<T> iterator = T.findAll(type);
+
+        if (!iterator.hasNext()) {
+            return null;
+        }
+
+        return iterator.next();
     }
 
     void delete(T entity) {
-
+        entity.delete();
     }
 
-    void deleteAll() {
-
+    void deleteAll(Class<T> type) {
+        T.deleteAll(type);
     }
 }
