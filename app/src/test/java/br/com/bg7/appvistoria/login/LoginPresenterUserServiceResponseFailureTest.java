130bb47c50b0ff707cd389971f9706e64d946b1c
elison <elison@voopter.com>
2017-07-25 13:48:54 -0300

    Criado o teste shouldShowMainScreenWhenNoBodyButUserAndPasswordOk Coluna 3 da linha 4.2

--- app/src/test/java/br/com/bg7/appvistoria/login/LoginPresenterUserServiceResponseFailureTest.java
+++ app/src/test/java/br/com/bg7/appvistoria/login/LoginPresenterUserServiceResponseFailureTest.java
@@ -141,4 +141,25 @@ public class LoginPresenterUserServiceResponseFailureTest extends LoginPresenter
         verify(loginView).showMainScreen();
     }
 
+    @Test
+    public void shouldShowMainScreenWhenNoBodyButUserAndPasswordOk() {
+        when(loginView.isConnected()).thenReturn(true);
+        when(userRepository.findByUsername(anyString())).thenReturn(new User());
+        loginPresenter.checkpw = true;
+        when(tokenHttpResponse.isSuccessful()).thenReturn(true);
+        Token token = new Token(TOKEN, USER_ID);
+        token.setExpiresIn(0);
+        when(tokenHttpResponse.body()).thenReturn(token);
+        when(userHttpResponse.isSuccessful()).thenReturn(false);
+        when(userHttpResponse.body()).thenReturn(null);
+
+        callLogin();
+
+        verify(tokenService).getToken(matches(USERNAME), matches(PASSWORD), tokenCallBackCaptor.capture());
+        tokenCallBackCaptor.getValue().onResponse(tokenHttpResponse);
+        verify(userService).getUser(matches(TOKEN), matches(USER_ID), userCallBackCaptor.capture());
+        userCallBackCaptor.getValue().onResponse(userHttpResponse);
+        verify(loginView).showMainScreen();
+    }
+
 }
