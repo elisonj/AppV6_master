7ea89d01a6929bc24bfc6ba389f7197f876d77f6
elison <elison@voopter.com>
2017-07-25 11:29:42 -0300

    Criado a classe LoginPresenterUserServiceResponseFailureTest e os testes da Coluna 1 e 2 da linha 4.1

--- app/src/test/java/br/com/bg7/appvistoria/login/LoginPresenterUserServiceResponseFailureTest.java
+++ app/src/test/java/br/com/bg7/appvistoria/login/LoginPresenterUserServiceResponseFailureTest.java
@@ -9,6 +9,7 @@ import br.com.bg7.appvistoria.data.source.remote.HttpCallback;
 import br.com.bg7.appvistoria.data.source.remote.HttpResponse;
 import br.com.bg7.appvistoria.data.source.remote.dto.Token;
 import br.com.bg7.appvistoria.data.source.remote.dto.UserResponse;
+import br.com.bg7.appvistoria.vo.User;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.matches;
@@ -52,6 +53,28 @@ public class LoginPresenterUserServiceResponseFailureTest extends LoginPresenter
         verify(userService).getUser(matches(TOKEN), matches(USER_ID), userCallBackCaptor.capture());
         userCallBackCaptor.getValue().onFailure(new Exception());
         verify(loginView).showCannotLoginError();
+    }
+
+    @Test
+    public void shouldShowMainScreenWhenNoSuccessAndBadPassword() {
+        when(loginView.isConnected()).thenReturn(true);
+        when(userRepository.findByUsername(anyString())).thenReturn(new User());
+        loginPresenter.checkpw = false;
+        when(tokenHttpResponse.isSuccessful()).thenReturn(true);
+        Token token = new Token(TOKEN, USER_ID);
+        token.setExpiresIn(0);
+        when(tokenHttpResponse.body()).thenReturn(token);
+        when(userHttpResponse.isSuccessful()).thenReturn(true);
+        UserResponse userResponse = new UserResponse();
+        when(userHttpResponse.body()).thenReturn(userResponse);
+
+        callLogin();
+
+        verify(tokenService).getToken(matches(USERNAME), matches(PASSWORD), tokenCallBackCaptor.capture());
+        tokenCallBackCaptor.getValue().onResponse(tokenHttpResponse);
+        verify(userService).getUser(matches(TOKEN), matches(USER_ID), userCallBackCaptor.capture());
+        userCallBackCaptor.getValue().onFailure(new Exception());
+        verify(loginView).showMainScreen();
     }
 
 
