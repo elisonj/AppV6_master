3c7490e9f2d74262889d0d83602b98d048607e37
elison <elison@voopter.com>
2017-07-27 18:08:35 -0300

    Inicio da implementação do HttpProgressCallback

---
+++ app/src/main/java/br/com/bg7/appvistoria/data/source/remote/SyncCallback.java
@@ -0,0 +1,13 @@
+package br.com.bg7.appvistoria.data.source.remote;
+
+import br.com.bg7.appvistoria.data.ProductInspection;
+
+/**
+ * Created by: elison
+ * Date: 2017-07-27
+ */
+public interface SyncCallback {
+    void onSuccess(ProductInspection productInspection);
+    void onProgressUpdated(ProductInspection productInspection, double progress);
+    void onFailure(ProductInspection productInspection, Throwable t);
+}
