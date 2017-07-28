c334a71e89d5c71b3e7892890ac2fa8cf7b01f21
elison <elison@voopter.com>
2017-07-28 17:36:26 -0300

    Removido método desnecessário

--- app/src/main/java/br/com/bg7/appvistoria/data/source/remote/retrofit/RetrofitHttpCall.java
+++ app/src/main/java/br/com/bg7/appvistoria/data/source/remote/retrofit/RetrofitHttpCall.java
@@ -3,7 +3,6 @@ package br.com.bg7.appvistoria.data.source.remote.retrofit;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import br.com.bg7.appvistoria.data.source.remote.HttpCallback;
-import br.com.bg7.appvistoria.data.source.remote.HttpProgressCallback;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -22,22 +21,6 @@ class RetrofitHttpCall<T> {
     }
 
     void enqueue(final HttpCallback<T> httpCallback) {
-        call.enqueue(new Callback<T>() {
-            @Override
-            @ParametersAreNonnullByDefault
-            public void onResponse(Call<T> call, Response<T> response) {
-                httpCallback.onResponse(new RetrofitHttpResponse<>(response));
-            }
-
-            @Override
-            @ParametersAreNonnullByDefault
-            public void onFailure(Call<T> call, Throwable t) {
-                httpCallback.onFailure(t);
-            }
-        });
-    }
-
-    void enqueue(final HttpProgressCallback<T> httpCallback) {
         call.enqueue(new Callback<T>() {
             @Override
             @ParametersAreNonnullByDefault
