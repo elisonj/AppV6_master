b8db8bcac8a112ca7a41a7c6d43f6f2484f30dde
elison <elison@voopter.com>
2017-07-28 15:00:00 -0300

    Alterado o nome da HttpProgress para ProgressRequestBody

--- app/src/main/java/br/com/bg7/appvistoria/data/source/remote/HttpProgressCallback.java
+++ app/src/main/java/br/com/bg7/appvistoria/data/source/remote/HttpProgressCallback.java
@@ -4,8 +4,6 @@ package br.com.bg7.appvistoria.data.source.remote;
  * Created by: elison
  * Date: 2017-07-28
  */
-public interface HttpProgressCallback<T> {
+public interface HttpProgressCallback<T> extends HttpCallback<T>{
     void onProgressUpdate(int percentage);
-    void onError(Throwable t);
-    void onFinish(HttpResponse<T> httpResponse);
 }
