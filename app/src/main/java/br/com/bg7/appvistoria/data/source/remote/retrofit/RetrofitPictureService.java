bce54bff9e8359916102c2aca37ceb19870a15dd
elison <elison@voopter.com>
2017-07-28 15:01:58 -0300

    Alterado o nome da RetrofitHttpProgressService para RetrofitPictureService

--- app/src/main/java/br/com/bg7/appvistoria/data/source/remote/retrofit/RetrofitPictureService.java
+++ app/src/main/java/br/com/bg7/appvistoria/data/source/remote/retrofit/RetrofitPictureService.java
@@ -3,7 +3,6 @@ package br.com.bg7.appvistoria.data.source.remote.retrofit;
 import java.io.File;
 
 import br.com.bg7.appvistoria.data.ProductInspection;
-import br.com.bg7.appvistoria.data.source.remote.HttpProgress;
 import br.com.bg7.appvistoria.data.source.remote.HttpProgressCallback;
 import br.com.bg7.appvistoria.data.source.remote.dto.PictureResponse;
 import okhttp3.MultipartBody;
@@ -16,13 +15,13 @@ import retrofit2.http.Part;
  * Created by: elison
  * Date: 2017-07-28
  */
-public class RetrofitHttpProgressService implements PictureService {
+public class RetrofitPictureService implements PictureService {
 
     private final PictureService pictureService;
 
     ProductInspection productInspection;
 
-    public RetrofitHttpProgressService(String baseUrl, ProductInspection productInspection) {
+    public RetrofitPictureService(String baseUrl, ProductInspection productInspection) {
         this.productInspection = productInspection;
         this.pictureService = new Retrofit.Builder()
                 .baseUrl(baseUrl)
@@ -33,7 +32,7 @@ public class RetrofitHttpProgressService implements PictureService {
     @Override
     public Call<PictureResponse> send(File attachment, @Part MultipartBody.Part file, ProductInspection productInspection, HttpProgressCallback httpProgressCallback) {
 
-        HttpProgress fileBody = new HttpProgress(attachment, httpProgressCallback);
+        ProgressRequestBody fileBody = new ProgressRequestBody(attachment, httpProgressCallback);
         MultipartBody.Part filePart = MultipartBody.Part.createFormData("image", attachment.getName(), fileBody);
 
         Call<PictureResponse> call = pictureService.send(attachment, filePart, productInspection, httpProgressCallback);
