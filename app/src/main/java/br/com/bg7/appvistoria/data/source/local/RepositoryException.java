47a14cacb66bf9f7abbc0184a15dc288bd0609dd
Lucio Assis <lucio.assis@bg7.com.br>
2017-07-28 18:05:54 -0300

    Documentar erros do HttpCallback

---
+++ app/src/main/java/br/com/bg7/appvistoria/data/source/local/RepositoryException.java
@@ -0,0 +1,9 @@
+package br.com.bg7.appvistoria.data.source.local;
+
+/**
+ * Created by: luciolucio
+ * Date: 2017-07-28
+ */
+
+public class RepositoryException {
+}
--- app/src/main/java/br/com/bg7/appvistoria/data/source/remote/HttpCallback.java
+++ app/src/main/java/br/com/bg7/appvistoria/data/source/remote/HttpCallback.java
@@ -7,5 +7,14 @@ package br.com.bg7.appvistoria.data.source.remote;
 
 public interface HttpCallback<T> {
     void onResponse(HttpResponse<T> httpResponse);
+
+    /**
+     * Invoked when a network exception occurred talking to the server or when an unexpected
+     * exception occurred creating the request or processing the response.
+     *
+     * @param t A throwable that can be whether:
+     * {@link java.io.IOException} if a problem occurred talking to the server.
+     * {@link RuntimeException} (and subclasses) if an unexpected error occurs creating the request
+     */
     void onFailure(Throwable t);
 }
