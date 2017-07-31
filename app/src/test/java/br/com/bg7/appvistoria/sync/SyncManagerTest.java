c5d557f86b52995bb43956b84f7ed1c4ca4b7381
Lucio Assis <lucio.assis@bg7.com.br>
2017-07-31 04:34:08 -0300

    Criar classe SyncManager

---
+++ app/src/test/java/br/com/bg7/appvistoria/sync/SyncManagerTest.java
@@ -0,0 +1,12 @@
+package br.com.bg7.appvistoria.sync;
+
+import org.junit.Test;
+
+/**
+ * Created by: luciolucio
+ * Date: 2017-07-31
+ */
+
+public class SyncManagerTest {
+    private SyncManager syncManager;
+}
