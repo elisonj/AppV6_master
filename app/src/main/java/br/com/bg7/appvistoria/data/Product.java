1d9d3dcb90070f5ee97428c85683818dde79f092
elison <elison@voopter.com>
2017-08-01 17:20:56 -0300

    Criado classe produto e inicio do metodo sync

---
+++ app/src/main/java/br/com/bg7/appvistoria/data/Product.java
@@ -0,0 +1,8 @@
+package br.com.bg7.appvistoria.data;
+
+/**
+ * Created by: elison
+ * Date: 2017-08-01
+ */
+public class Product {
+}
--- app/src/main/java/br/com/bg7/appvistoria/data/ProductInspection.java
+++ app/src/main/java/br/com/bg7/appvistoria/data/ProductInspection.java
@@ -2,15 +2,44 @@ package br.com.bg7.appvistoria.data;
 
 import com.orm.SugarRecord;
 
+import br.com.bg7.appvistoria.data.source.ProductInspectionService;
+import br.com.bg7.appvistoria.data.source.remote.HttpProgressCallback;
+import br.com.bg7.appvistoria.data.source.remote.HttpResponse;
+import br.com.bg7.appvistoria.data.source.remote.SyncCallback;
+import br.com.bg7.appvistoria.data.source.remote.dto.ProductResponse;
+
 /**
  * Created by: elison
  * Date: 2017-07-27
  */
 public class ProductInspection extends SugarRecord<ProductInspection> {
 
+    private long id;
+    private Product product;
+
     /**
      * Default constructor used by Sugar
      */
     @SuppressWarnings("unused")
     public ProductInspection() {}
+
+    public void sync(ProductInspectionService productInspectionService, final SyncCallback syncCallback) {
+
+        productInspectionService.send(this, new HttpProgressCallback<ProductResponse>() {
+            @Override
+            public void onProgressUpdated(double percentage) {
+                syncCallback.onProgressUpdated(ProductInspection.this, percentage);
+            }
+
+            @Override
+            public void onResponse(HttpResponse<ProductResponse> httpResponse) {
+                syncCallback.onSuccess(ProductInspection.this);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                syncCallback.onFailure(ProductInspection.this, t);
+            }
+        });
+    }
 }
