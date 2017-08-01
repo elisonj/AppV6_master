63f7a13df646afa53bcf84ac4a1bbd978e4036af
Lucio Assis <lucio.assis@bg7.com.br>
2017-08-01 14:38:14 -0300

    HttpProgressCallbackTest -> TestHttpProgressCallback

--- app/src/test/java/br/com/bg7/appvistoria/data/source/remote/TestHttpProgressCallback.java
+++ app/src/test/java/br/com/bg7/appvistoria/data/source/remote/TestHttpProgressCallback.java
@@ -4,7 +4,7 @@ package br.com.bg7.appvistoria.data.source.remote;
  * Created by: elison
  * Date: 2017-07-31
  */
-public abstract class HttpProgressCallbackTest implements HttpProgressCallback {
+public abstract class TestHttpProgressCallback implements HttpProgressCallback {
 
     @Override
     public void onFailure(Throwable t) {
