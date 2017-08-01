5bcabe049a155b61df32a7f5b914711ac0f8e1c4
Lucio Assis <lucio.assis@bg7.com.br>
2017-08-01 16:23:40 -0300

    A logica basica do SyncManager

--- app/src/main/java/br/com/bg7/appvistoria/sync/SyncExecutor.java
+++ app/src/main/java/br/com/bg7/appvistoria/sync/SyncExecutor.java
@@ -1,5 +1,7 @@
 package br.com.bg7.appvistoria.sync;
 
+import br.com.bg7.appvistoria.data.ProductInspection;
+
 /**
  * Created by: luciolucio
  * Date: 2017-07-31
@@ -9,4 +11,6 @@ interface SyncExecutor {
     void scheduleQueueUpdates(Runnable runnable);
 
     void scheduleSyncLoop(Runnable runnable);
+
+    boolean executeSync(ProductInspection inspection);
 }
