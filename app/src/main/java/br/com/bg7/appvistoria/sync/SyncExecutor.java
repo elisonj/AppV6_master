01296e1829f25f366398445d42d07279b9fba50c
Lucio Assis <lucio.assis@bg7.com.br>
2017-08-02 11:05:19 -0300

    Documentar SyncExecutor

--- app/src/main/java/br/com/bg7/appvistoria/sync/SyncExecutor.java
+++ app/src/main/java/br/com/bg7/appvistoria/sync/SyncExecutor.java
@@ -8,9 +8,32 @@ import br.com.bg7.appvistoria.data.ProductInspection;
  */
 
 interface SyncExecutor {
+    /**
+     * Implementers must schedule the {@link Runnable} to run on a separate thread
+     * and keep running it periodically
+     *
+     * @param runnable
+     */
     void scheduleQueueUpdates(Runnable runnable);
 
+    /**
+     * Implementers must schedule the {@link Runnable} to run on a separate thread
+     * and keep running it periodically
+     *
+     * @param runnable
+     */
     void scheduleSyncLoop(Runnable runnable);
 
-    boolean executeSync(ProductInspection inspection, Runnable runnable);
+    /**
+     * Implementers must check the {@link ProductInspection} and decide whether
+     * they are capable of syncing either a product or a picture. If they are,
+     * they must spawn a thread to run the {@link Runnable} and return true.
+     *
+     * If not, ignore the {@code Runnable} and return false.
+     *
+     * @param inspection  the inspection that is going to get synced
+     * @param syncJob  the sync code to run
+     * @return true if the job can be run, false if not
+     */
+    boolean executeSync(ProductInspection inspection, Runnable syncJob);
 }
