9b912ed386149f545650ca9490013b65cf1a71a1
elison <elison@voopter.com>
2017-08-02 17:00:03 -0300

    alterado o callback do ProductInspection para uma classe separa e criado o teste para ver se foi sincronizado

--- app/src/main/java/br/com/bg7/appvistoria/data/ProductInspection.java
+++ app/src/main/java/br/com/bg7/appvistoria/data/ProductInspection.java
@@ -13,7 +13,6 @@ import br.com.bg7.appvistoria.data.source.remote.HttpProgressCallback;
 import br.com.bg7.appvistoria.data.source.remote.HttpResponse;
 import br.com.bg7.appvistoria.data.source.remote.SyncCallback;
 import br.com.bg7.appvistoria.data.source.remote.dto.PictureResponse;
-import br.com.bg7.appvistoria.data.source.remote.dto.ProductResponse;
 import br.com.bg7.appvistoria.sync.SyncStatus;
 
 /**
@@ -23,6 +22,7 @@ import br.com.bg7.appvistoria.sync.SyncStatus;
 public class ProductInspection extends SugarRecord<ProductInspection> {
 
     private SyncStatus syncStatus = null;
+    private ProductInspectionCallback productInspectionCallback;
 
     private long id;
     private Product product;
@@ -36,6 +36,10 @@ public class ProductInspection extends SugarRecord<ProductInspection> {
 
     public SyncStatus getSyncStatus() {
         return syncStatus;
+    }
+
+    public void setSyncStatus(SyncStatus syncStatus) {
+        this.syncStatus = syncStatus;
     }
 
     public boolean canSyncProduct() {
@@ -78,28 +82,18 @@ public class ProductInspection extends SugarRecord<ProductInspection> {
     }
 
     public void sync(ProductInspectionService productInspectionService, final SyncCallback syncCallback) {
-
-        if(syncStatus == SyncStatus.READY) {
-            syncStatus = SyncStatus.PRODUCT_INSPECTION_BEING_SYNCED;
-
-            productInspectionService.send(this, new HttpProgressCallback<ProductResponse>() {
-                @Override
-                public void onProgressUpdated(double percentage) {
-                    syncCallback.onProgressUpdated(ProductInspection.this, percentage);
-                }
-
-                @Override
-                public void onResponse(HttpResponse<ProductResponse> httpResponse) {
-                    syncStatus = SyncStatus.PRODUCT_INSPECTION_SYNCED;
-                    syncCallback.onSuccess(ProductInspection.this);
-                }
-
-                @Override
-                public void onFailure(Throwable t) {
-                    syncStatus = SyncStatus.FAILED;
-                    syncCallback.onFailure(ProductInspection.this, t);
-                }
-            });
+        try {
+            if (ready() == SyncStatus.READY) {
+                syncStatus = SyncStatus.PRODUCT_INSPECTION_BEING_SYNCED;
+                productInspectionCallback = new ProductInspectionCallback(this, syncCallback);
+                productInspectionService.send(this, productInspectionCallback);
+            }
+        } catch (SyncFailedException ex) {
+            ex.printStackTrace();;
         }
+    }
+
+    public ProductInspectionCallback getProductInspectionCallback() {
+        return productInspectionCallback;
     }
 }
--- app/src/test/java/br/com/bg7/appvistoria/data/ProductInspectionTest.java
+++ app/src/test/java/br/com/bg7/appvistoria/data/ProductInspectionTest.java
@@ -4,16 +4,18 @@ import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.io.SyncFailedException;
 
+import javax.annotation.Nullable;
+
 import br.com.bg7.appvistoria.data.source.PictureService;
 import br.com.bg7.appvistoria.data.source.ProductInspectionService;
+import br.com.bg7.appvistoria.data.source.remote.HttpResponse;
 import br.com.bg7.appvistoria.data.source.remote.SyncCallback;
+import br.com.bg7.appvistoria.data.source.remote.dto.ProductResponse;
 import br.com.bg7.appvistoria.sync.SyncStatus;
 
 /**
@@ -23,6 +25,7 @@ import br.com.bg7.appvistoria.sync.SyncStatus;
 public class ProductInspectionTest {
 
     ProductInspection productInspection;
+    ProductInspectionCallback productInspectionCallback;
 
     @Mock
     ProductInspectionService productInspectionService;
@@ -30,21 +33,53 @@ public class ProductInspectionTest {
     @Mock
     PictureService pictureService;
 
-    @Captor
-    ArgumentCaptor<SyncCallback> productCallBackCaptor;
+
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         productInspection = new ProductInspection();
+        productInspectionCallback = new ProductInspectionCallback(productInspection, callback);
     }
 
     @Test
     public void shouldSyncProductInspection() throws SyncFailedException{
         Assert.assertEquals(productInspection.ready(), SyncStatus.READY);
 
-        productInspection.sync(productInspectionService, productCallBackCaptor.capture());
-        productCallBackCaptor.getValue().onSuccess(productInspection);
+        productInspection.sync(productInspectionService, callback);
+        productInspection.getProductInspectionCallback().onResponse(productResponse);
+
         Assert.assertEquals(productInspection.getSyncStatus(), SyncStatus.PRODUCT_INSPECTION_SYNCED);
     }
+
+    private SyncCallback callback = new SyncCallback() {
+        @Override
+        public void onSuccess(ProductInspection productInspection) { }
+
+        @Override
+        public void onProgressUpdated(ProductInspection productInspection, double progress) { }
+
+        @Override
+        public void onFailure(ProductInspection productInspection, Throwable t) { }
+    };
+
+    private HttpResponse<ProductResponse> productResponse = new HttpResponse<ProductResponse>() {
+        @Override
+        public boolean isSuccessful() {
+            return true;
+        }
+
+        @Nullable
+        @Override
+        public ProductResponse body() {
+            return null;
+        }
+
+        @Override
+        public int code() {
+            return 0;
+        }
+    };
+
+
 }
