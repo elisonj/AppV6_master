2ad37fb010a758cced7efe9e0faaabbc0fabc5dc
Lucio Assis <lucio.assis@bg7.com.br>
2017-08-02 11:04:24 -0300

    Criar SyncManagerTestBase

--- app/src/test/java/br/com/bg7/appvistoria/sync/SyncManagerInitializationTest.java
+++ app/src/test/java/br/com/bg7/appvistoria/sync/SyncManagerInitializationTest.java
@@ -1,46 +1,16 @@
 package br.com.bg7.appvistoria.sync;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-
-import br.com.bg7.appvistoria.data.source.PictureService;
-import br.com.bg7.appvistoria.data.source.ProductInspectionService;
-import br.com.bg7.appvistoria.data.source.local.ProductInspectionRepository;
-import br.com.bg7.appvistoria.data.source.local.fake.FakeProductInspectionRepository;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
 /**
  * Created by: luciolucio
- * Date: 2017-07-31
+ * Date: 2017-08-02
  */
 
-public class SyncManagerTest {
-    private SyncManager syncManager;
-
-    private FakeProductInspectionRepository productInspectionRepository;
-
-    @Mock
-    SyncExecutor syncExecutor;
-
-    @Mock
-    ProductInspectionService productInspectionService;
-
-    @Mock
-    PictureService pictureService;
-
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-
-        productInspectionRepository = new FakeProductInspectionRepository();
-        syncManager = new SyncManager(productInspectionRepository, productInspectionService, pictureService, syncExecutor);
-    }
+public class SyncManagerInitializationTest extends SyncManagerTestBase {
 
     @Test(expected = NullPointerException.class)
     public void shouldNotAcceptNullRepository() {
---
+++ app/src/test/java/br/com/bg7/appvistoria/sync/SyncManagerTestBase.java
@@ -0,0 +1,44 @@
+package br.com.bg7.appvistoria.sync;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+import br.com.bg7.appvistoria.data.source.PictureService;
+import br.com.bg7.appvistoria.data.source.ProductInspectionService;
+import br.com.bg7.appvistoria.data.source.local.ProductInspectionRepository;
+import br.com.bg7.appvistoria.data.source.local.fake.FakeProductInspectionRepository;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by: luciolucio
+ * Date: 2017-07-31
+ */
+
+public class SyncManagerTestBase {
+    SyncManager syncManager;
+
+    FakeProductInspectionRepository productInspectionRepository;
+
+    @Mock
+    SyncExecutor syncExecutor;
+
+    @Mock
+    ProductInspectionService productInspectionService;
+
+    @Mock
+    PictureService pictureService;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        productInspectionRepository = new FakeProductInspectionRepository();
+        syncManager = new SyncManager(productInspectionRepository, productInspectionService, pictureService, syncExecutor);
+    }
+}
