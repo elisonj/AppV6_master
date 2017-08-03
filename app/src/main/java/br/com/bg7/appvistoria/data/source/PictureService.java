a660cbb5c2ea9ffc89f8bf285b0424bec6a0c8ef
Lucio Assis <lucio.assis@bg7.com.br>
2017-08-03 11:34:40 -0300

    +TODO

--- app/src/main/java/br/com/bg7/appvistoria/data/source/PictureService.java
+++ app/src/main/java/br/com/bg7/appvistoria/data/source/PictureService.java
@@ -9,6 +9,8 @@ import br.com.bg7.appvistoria.data.source.remote.dto.PictureResponse;
 /**
  * Created by: elison
  * Date: 2017-07-28
+ *
+ * TODO: Mover todos os Service para dentro de remote
  */
 public interface PictureService {
     void send(File attachment,
