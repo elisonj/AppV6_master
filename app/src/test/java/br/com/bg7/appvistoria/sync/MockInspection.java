c33fdcb1bf8be296182fa62505c7bcdb573c03eb
Lucio Assis <lucio.assis@bg7.com.br>
2017-08-04 20:24:42 -0300

    Metodos para throw de IllegalStateException

--- app/src/test/java/br/com/bg7/appvistoria/sync/MockInspection.java
+++ app/src/test/java/br/com/bg7/appvistoria/sync/MockInspection.java
@@ -1,7 +1,12 @@
 package br.com.bg7.appvistoria.sync;
 
 import br.com.bg7.appvistoria.data.ProductInspection;
+import br.com.bg7.appvistoria.data.source.PictureService;
+import br.com.bg7.appvistoria.data.source.ProductInspectionService;
+import br.com.bg7.appvistoria.data.source.remote.SyncCallback;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -20,28 +25,49 @@ class MockInspection {
     }
 
     MockInspection withStatus(SyncStatus syncStatus) {
-        when(this.productInspection.getSyncStatus()).thenReturn(syncStatus);
+        when(productInspection.getSyncStatus()).thenReturn(syncStatus);
         return this;
     }
 
     MockInspection thatCanSyncPictures() {
-        when(this.productInspection.canSyncPictures()).thenReturn(true);
+        when(productInspection.canSyncPictures()).thenReturn(true);
         return this;
     }
 
     MockInspection thatCannotSyncPictures() {
-        when(this.productInspection.canSyncPictures()).thenReturn(false);
+        when(productInspection.canSyncPictures()).thenReturn(false);
         return this;
     }
 
     MockInspection thatCanSyncProduct() {
-        when(this.productInspection.canSyncProduct()).thenReturn(true);
+        when(productInspection.canSyncProduct()).thenReturn(true);
         return this;
     }
 
     MockInspection thatCannotSyncProduct() {
-        when(this.productInspection.canSyncProduct()).thenReturn(false);
+        when(productInspection.canSyncProduct()).thenReturn(false);
         return this;
+    }
+
+    MockInspection thatThrowsOnProductSync() {
+        MockInspection inspection = this
+                .thatCannotSyncPictures()
+                .thatCanSyncProduct();
+
+        doThrow(new IllegalStateException()).when(inspection.create())
+                .sync(any(ProductInspectionService.class), any(SyncCallback.class));
+
+        return inspection;
+    }
+
+    MockInspection thatThrowsOnPictureSync() {
+        MockInspection inspection = this
+                .thatCanSyncPictures();
+
+        doThrow(new IllegalStateException()).when(inspection.create())
+                .sync(any(PictureService.class), any(SyncCallback.class));
+
+        return inspection;
     }
 
     ProductInspection create() {
