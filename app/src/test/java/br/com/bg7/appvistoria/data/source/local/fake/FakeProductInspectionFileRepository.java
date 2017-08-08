d31e189fc871f39ff058fa9d2064929e91c19c4d
elison <elison@voopter.com>
2017-08-08 14:28:50 -0300

    removido metodos de busca de ProductFile

--- app/src/test/java/br/com/bg7/appvistoria/data/source/local/fake/FakeProductInspectionFileRepository.java
+++ app/src/test/java/br/com/bg7/appvistoria/data/source/local/fake/FakeProductInspectionFileRepository.java
@@ -1,20 +1,9 @@
 package br.com.bg7.appvistoria.data.source.local.fake;
 
-import com.google.common.base.Predicate;
-import com.google.common.collect.Maps;
-
-import java.util.Map;
-
-import javax.annotation.Nullable;
-
 import br.com.bg7.appvistoria.data.ProductFile;
 import br.com.bg7.appvistoria.data.source.local.ProductFileRepository;
 import br.com.bg7.appvistoria.sync.SyncStatus;
 
-/**
- * Created by: luciolucio
- * Date: 2017-07-31
- */
 
 public class FakeProductInspectionFileRepository extends FakeRepository<SyncStatus, ProductFile> implements ProductFileRepository {
 
@@ -22,23 +11,5 @@ public class FakeProductInspectionFileRepository extends FakeRepository<SyncStat
     SyncStatus getKey(ProductFile entity) {
         return entity.getSyncStatus();
     }
-
-    @Override
-    public Iterable<ProductFile> findByProductInspectionIdAndSyncStatus(long productInspectionId, final SyncStatus status) {
-        Map<SyncStatus, ProductFile> filtered = Maps.filterEntries(ENTITIES_BY_KEY, new Predicate<Map.Entry<SyncStatus, ProductFile>>() {
-            @Override
-            public boolean apply(@Nullable Map.Entry<SyncStatus, ProductFile> input) {
-                return input != null && input.getKey().equals(status);
-            }
-        });
-
-        return filtered.values();
-    }
-
-    @Override
-    public Iterable<ProductFile> findByProductInspectionId(long productInspectionId) {
-        return null;
-    }
-
 
 }
