3468c46be89a7f2a96e40ae51de1968e2be5691b
elison <elison@voopter.com>
2017-08-14 10:12:39 -0300

    Criado teste de interface para quando a usuario for errado

---
+++ app/src/androidTest/java/br/com/bg7/appvistoria/login/ShouldShowErrorWhenUserIsWrong.java
@@ -0,0 +1,70 @@
+package br.com.bg7.appvistoria.login;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.com.bg7.appvistoria.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ShouldShowErrorWhenUserIsWrong {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void shouldShowErrorWhenUserIsWrong() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.editText_user), isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.editText_user), isDisplayed()));
+        appCompatEditText2.perform(replaceText("user"), closeSoftKeyboard());
+
+        pressBack();
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.editText_password), isDisplayed()));
+        appCompatEditText3.perform(replaceText("pass"), closeSoftKeyboard());
+
+        pressBack();
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.button_login), withText("ENTRAR"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(android.R.id.button1), withText("OK")));
+        appCompatButton2.perform(scrollTo(), click());
+
+    }
+
+}
