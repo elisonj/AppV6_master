e3a1dcbf9c86ef1b77e5213346780e8c4f504f7e
elison <elison@voopter.com>
2017-08-15 10:44:39 -0300

    WorkOrder deve extender SugarRecord

--- app/src/main/java/br/com/bg7/appvistoria/data/WorkOrder.java
+++ app/src/main/java/br/com/bg7/appvistoria/data/WorkOrder.java
@@ -1,5 +1,7 @@
 package br.com.bg7.appvistoria.data;
 
+import com.orm.SugarRecord;
+
 import org.joda.time.DateTime;
 
 import java.util.List;
@@ -10,7 +12,7 @@ import br.com.bg7.appvistoria.workorder.WorkOrderStatus;
  * Created by: elison
  * Date: 2017-08-15
  */
-public class WorkOrder {
+public class WorkOrder extends SugarRecord<WorkOrder> {
     private String name;
     private String sumary;
     private String shortSumary;
@@ -19,4 +21,11 @@ public class WorkOrder {
     private String address;
     private Long externalId;
     private List<Inspection> inspections;
+
+    /**
+     * Default constructor used by Sugar
+     */
+    @SuppressWarnings("unused")
+    public WorkOrder() {}
+
 }
