9e88d03e65d4ec0610b57198ef65dba387368722
elison <elison@voopter.com>
2017-08-15 15:38:19 -0300

    implementação do metodo search ( buscando do banco e ordenando os resultados )

---
+++ app/src/main/java/br/com/bg7/appvistoria/workorder/WorkOrderPresenter.java
@@ -0,0 +1,51 @@
+package br.com.bg7.appvistoria.workorder;
+
+import java.util.List;
+
+import br.com.bg7.appvistoria.auth.Auth;
+import br.com.bg7.appvistoria.data.Config;
+import br.com.bg7.appvistoria.data.WorkOrder;
+import br.com.bg7.appvistoria.data.source.local.ConfigRepository;
+import br.com.bg7.appvistoria.data.source.local.WorkOrderRepository;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Created by: elison
+ * Date: 2017-08-15
+ */
+public class WorkOrderPresenter implements WorkOrderContract.Presenter{
+
+    private final WorkOrderRepository workOrderRepository;
+    private final WorkOrderContract.View workOrderView;
+    private final ConfigRepository configRepository;
+
+    private String language;
+
+    public WorkOrderPresenter(WorkOrderRepository workOrderRepository, WorkOrderContract.View workOrderView,
+                       ConfigRepository configRepository) {
+        this.workOrderRepository = checkNotNull(workOrderRepository);
+        this.workOrderView = checkNotNull(workOrderView);
+        this.configRepository = checkNotNull(configRepository);
+
+        this.workOrderView.setPresenter(this);
+    }
+
+
+    @Override
+    public void start() {
+
+        Config config = configRepository.findByUser(Auth.user());
+        if(config == null) {
+            return;
+        }
+
+        language = config.getLanguageName();
+    }
+
+    @Override
+    public void search(String searchTerm) {
+        List<WorkOrder> workOrderList =  workOrderRepository.findAllOrderByStatus(language);
+        workOrderView.showList(workOrderList, true);
+    }
+}
