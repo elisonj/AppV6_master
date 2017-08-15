2f61c0c8b3a0984f1293cd778b1101f162c83282
elison <elison@voopter.com>
2017-08-15 10:37:30 -0300

    classe de status da visita

---
+++ app/src/main/java/br/com/bg7/appvistoria/workorder/WorkOrderStatus.java
@@ -0,0 +1,11 @@
+package br.com.bg7.appvistoria.workorder;
+
+/**
+ * Created by: elison
+ * Date: 2017-08-15
+ */
+public enum WorkOrderStatus {
+    NOT_STARTED,
+    IN_PROGRESS,
+    COMPLETED
+}
