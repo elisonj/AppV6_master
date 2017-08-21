1219b413d8b2a6e0ab93a11759365defda5d7a5e
elison <elison@voopter.com>
2017-08-21 11:07:28 -0300

    Implementacao do acesso ao Respositorio das Visitas

---
+++ app/src/main/java/br/com/bg7/appvistoria/data/source/local/ormlite/OrmLiteWorkOrderRepository.java
@@ -0,0 +1,41 @@
+package br.com.bg7.appvistoria.data.source.local.ormlite;
+
+import com.j256.ormlite.dao.RuntimeExceptionDao;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import br.com.bg7.appvistoria.data.WorkOrder;
+import br.com.bg7.appvistoria.data.source.local.WorkOrderRepository;
+
+/**
+ * Created by: elison
+ * Date: 2017-08-15
+ */
+public class OrmLiteWorkOrderRepository extends OrmLiteRepository<WorkOrder> implements WorkOrderRepository {
+
+    public OrmLiteWorkOrderRepository(RuntimeExceptionDao<WorkOrder, Long> dao) {
+        super(dao);
+    }
+
+    @Override
+    public List<WorkOrder> findAllOrderByStatus(String language) {
+        if(language == null)  return null;
+
+        List<WorkOrder> workOrderList = dao.queryForAll();
+        if (workOrderList.size() <= 0) {
+            return null;
+        }
+
+        Collections.sort(workOrderList, new Comparator<WorkOrder>() {
+            @Override
+            public int compare(WorkOrder workOrder, WorkOrder workOrder1) {
+                return workOrder.getStatus().compareTo(workOrder1.getStatus());
+            }
+        });
+
+        return workOrderList;
+    }
+}
+
