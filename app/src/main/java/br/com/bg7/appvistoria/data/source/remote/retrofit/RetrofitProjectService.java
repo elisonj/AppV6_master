50b07f2167689afa29c8c90a1031dc303893375c
Lucio Assis <lucio.assis@bg7.com.br>
2017-09-12 11:31:22 -0300

    Criar RetrofitLoggedInService para reuso

--- app/src/main/java/br/com/bg7/appvistoria/data/source/remote/retrofit/RetrofitProjectService.java
+++ app/src/main/java/br/com/bg7/appvistoria/data/source/remote/retrofit/RetrofitProjectService.java
@@ -2,39 +2,32 @@ package br.com.bg7.appvistoria.data.source.remote.retrofit;
 
 import java.util.List;
 
-import br.com.bg7.appvistoria.auth.Auth;
 import br.com.bg7.appvistoria.data.source.remote.http.HttpCallback;
 import br.com.bg7.appvistoria.data.source.remote.http.HttpResponse;
 import br.com.bg7.appvistoria.data.source.remote.retrofit.http.RetrofitHttpCall;
 import br.com.bg7.appvistoria.data.source.remote.retrofit.http.RetrofitProjectHttpResponse;
 import br.com.bg7.appvistoria.projectselection.vo.Project;
 import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by: elison
  * Date: 2017-09-04
  */
-public class RetrofitProjectService implements br.com.bg7.appvistoria.data.source.remote.ProjectService {
+public class RetrofitProjectService extends RetrofitLoggedInService<ProjectService> implements br.com.bg7.appvistoria.data.source.remote.ProjectService {
 
     private final static String SEARCH_KEY = "searchKey:";
-    private final static String KEY_TOKEN = "Bearer ";
 
     private final ProjectService projectService;
 
     public RetrofitProjectService(String baseUrl) {
+        super(baseUrl);
 
-        this.projectService = new Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build().create(ProjectService.class);
+        this.projectService = buildService(ProjectService.class);
     }
 
     @Override
     public void findByIdOrDescription(String idOrDescription, final HttpCallback<List<Project>> callback) {
-        String token = KEY_TOKEN + Auth.token();
-        Call<br.com.bg7.appvistoria.data.source.remote.dto.Project>  call = projectService.findByIdOrDescription(token, SEARCH_KEY + idOrDescription);
+        Call<br.com.bg7.appvistoria.data.source.remote.dto.Project>  call = projectService.findByIdOrDescription(TOKEN, SEARCH_KEY + idOrDescription);
         RetrofitHttpCall<br.com.bg7.appvistoria.data.source.remote.dto.Project> httpCall = new RetrofitHttpCall<>(call);
 
         httpCall.enqueue(new HttpCallback<br.com.bg7.appvistoria.data.source.remote.dto.Project>() {
@@ -43,7 +36,6 @@ public class RetrofitProjectService implements br.com.bg7.appvistoria.data.sourc
                 br.com.bg7.appvistoria.data.source.remote.dto.Project response = httpResponse.body();
                 List<Project> projects = br.com.bg7.appvistoria.data.source.remote.dto.Project.fromProjectResponse(response);
                 callback.onResponse(new RetrofitProjectHttpResponse(projects, httpResponse.isSuccessful(), httpResponse.code()));
-
             }
 
             @Override
